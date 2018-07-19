70a0a63eb4de1a921d4ea7be850fc66bb5aabdab
Obada Jabassini <obada.jabassini@gmail.com>
2018-07-19 21:43:52 +0300

    Handle hypernym case

--- object_recognition/object_recognition_model.py
+++ object_recognition/object_recognition_model.py
@@ -2,6 +2,7 @@ from collections import Counter
 
 import cv2
 import inflect
+from nltk.corpus import wordnet as wn
 
 from file_path_manager import FilePathManager
 from object_recognition.predictor.retina_net.retina_net_predictor import RetinaNetPredictor
@@ -11,19 +12,36 @@ class ObjectRecognitionModel:
     def __init__(self):
         self.predictor = RetinaNetPredictor()
         self.p = inflect.engine()
+        # self.word2vec = gensim.models.KeyedVectors.load(
+        #     FilePathManager.resolve("object_recognition/data/word2vec.model"))
 
-    def process_result(self, result):
-        counter = Counter(result)
+    def count_objects(self, counter):
         result = [f"{value} {self.p.plural(key, value)}" for key, value in counter.items()]
         return result
 
-    def predict(self, image):
-        result = self.predictor.predict(image)
-        result = self.process_result(result)
-        return ",".join(result)
+    def count_object(self, counter, object_name):
+        result = 0
+        obj = wn.synsets(object_name)[0]
+        for key, value in counter.items():
+            k = wn.synsets(key)[0]
+            hyper = set([i for i in k.closure(lambda s: s.hypernyms())])
+            if object_name == key or obj in hyper:
+                result += value
+        return result
+
+    def predict(self, image, object_name=""):
+        objects = self.predictor.predict(image)
+        counter = Counter(objects)
+        if object_name == "":
+            result = self.count_objects(counter)
+            result = ",".join(result)
+        else:
+            result = self.count_object(counter, object_name)
+            result = str(result)
+        return result
 
 
 if __name__ == '__main__':
     model = ObjectRecognitionModel()
-    image = cv2.imread(FilePathManager.resolve("vqa/test_images/doing.jpg"))
-    print(model.predict(image))
+    image = cv2.imread(FilePathManager.resolve("vqa/test_images/girl.jpg"))
+    print(model.predict(image, object_name="orange"))
